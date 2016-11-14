a4e559a62195eea9ac577d7881e46d1bb434914a
Teele Sepman <tsepman@labor-01-l.intra.itcollege.ee>
2016-11-14 20:24:51 +0200

    Add uart functions from https://github.com/tuupola/avr_demo

--- src/main.c
+++ src/main.c
@@ -1,16 +1,47 @@
+#include <stdio.h>
+#define __ASSERT_USE_STDERR
+#include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#define	BLINK_DELAY_MS 1000
-int	main (void)
+#include "uart.h"
+#define BLINK_DELAY_MS 100
+void main (void)
 {
-  /* set pin 3 of PORTA for output*/
+    /* Set pin 3 of PORTA for output */
     DDRA |= _BV(DDA3);
-    while(1) {
-      /* set pin 3 high to turn led on */
-      PORTA |= _BV(PORTA3);
-      _delay_ms(BLINK_DELAY_MS);
-      /* set pin 3 low to turn led off */
-      PORTA &= ~_BV(PORTA3);
-      _delay_ms(BLINK_DELAY_MS);
+    /* Init error console as stderr in UART3 and print user code info */
+    uart3_initialize();
+    stderr = &uart3_out;
+    fprintf(stderr, "Version: %s built on: %s %s\n",
+            GIT_DESCR, __DATE__, __TIME__);
+    fprintf(stderr, "avr-libc version: %s\n", __AVR_LIBC_VERSION_STRING__);
+    /* End UART3 init and info print */
+    /* Test assert - REMOVE IN FUTURE LABS */
+    char *array;
+    uint32_t i = 1;
+    extern int __heap_start, *__brkval;
+    int v;
+    array = malloc( i * sizeof(char));
+    assert(array);
+    /* End test assert */
+
+    while (1) {
+        /* Set pin 3 high to turn LED on */
+        PORTA |= _BV(PORTA3);
+        _delay_ms(BLINK_DELAY_MS);
+        /* Test assert - REMOVE IN FUTURE LABS */
+        /*
+        * Increase memory allocated for array by 100 chars
+        * until we have eaten it all and print space between Stack and Heap.
+        * Thats how assert works in run-time.
+        */
+        array = realloc( array, (i++ * 100) * sizeof(char));
+        fprintf(stderr, "%d ",
+                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
+        assert(array);
+        /* End test assert */
+        /* Set pin 3 low to turn LED off */
+        PORTA &= ~_BV(PORTA3);
+        _delay_ms(BLINK_DELAY_MS);
     }
 }
